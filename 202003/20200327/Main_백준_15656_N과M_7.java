e671246d7d5b670b311902046d9ed05173f8b003
ponyong <ponyong@naver.com>
2020-03-27 22:18:10 +0900

    BOJ 15656 N과M(7) ( simulation )

---
+++ 202003/20200327/Main_백준_15656_N과M_7.java
@@ -0,0 +1,44 @@
+package home;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_백준_15656_N과M_7 {
+	static int[] arr;
+	static int N, M;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer str = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(str.nextToken());
+		M = Integer.parseInt(str.nextToken());
+		
+		arr = new int[N];
+		
+		str = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(str.nextToken());
+		}
+		Arrays.sort(arr);
+		solve(0, "");
+	}
+
+	private static void solve(int cnt, String str) throws IOException {
+		
+		if(cnt ==  M) {
+			System.out.println(str);
+			return;
+		}
+		for(int i = 0; i < N; i++) {
+			solve(cnt + 1, str + arr[i] + " ");
+		}
+
+	}
+}
