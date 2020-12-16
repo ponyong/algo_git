7e7ed4da4f7d166caccb74f403f6305b0535f0ed
ponyong <ponyong@naver.com>
2020-12-16 23:43:23 +0900

    SWEA 암호생성기

---
+++ 2020/202012/Solution_암호생성기.java
@@ -0,0 +1,40 @@
+package solution;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution_암호생성기 {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			int T;
+			T = sc.nextInt();
+
+			int[] arr = new int[8];
+			int temp = 0;
+			for (int i = 0; i < 8; i++) {
+				arr[i] = sc.nextInt();
+			}
+			do {
+				for (int minus = 1; minus <= 5; minus++) {
+					temp = arr[0];
+
+					for (int j = 0; j < 7; j++) {
+						arr[j] = arr[j + 1];
+					}
+					if (temp - minus <= 0) {
+						arr[7] = 0;
+						break;
+					}
+					arr[7] = temp - minus;
+				}
+			} while (arr[7] > 0);
+			System.out.print("#" + T + " ");
+			for (int i = 0; i < 8; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
