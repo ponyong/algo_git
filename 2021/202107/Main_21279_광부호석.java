1332edba02b2542070ba0c6826c569ad9624de94
ponyong <ponyong@naver.com>
2021-07-27 23:27:21 +0900

    [BOJ] 광부호석

---
+++ 2021/202107/Main_21279_광부호석.java
@@ -0,0 +1,17 @@
+package Algo_Study_BOJ;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main_21279_광부호석 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer str = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(str.nextToken());
+		int C = Integer.parseInt(str.nextToken());
+		
+		
+	}
+}
